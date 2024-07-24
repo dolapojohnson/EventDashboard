77cf54ae25d86d3d219f560fbc701d842454cb12
dolapojohnson <dolapojohnson99@gmail.com>
2024-07-24 12:45:26 +0100

    Login Page UI implementation complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'
+import Login from './routes/SignIn/SignIn';
 
 function App() {
   return (
     <Routes>
-      
+      <Route path='/' index element={<Login />} />
     </Routes>
   );
 }
